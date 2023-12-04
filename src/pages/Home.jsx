25d02c73fd7ac39b0224603ecb2260a81e5a4de8
SuinChoi <ctd2015@gmail.com>
2023-12-04 12:19:06 +0900

    리액트 숙련주차 개인과제 3번까지 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,19 @@ import AddForm from "components/AddForm";
 import Header from "components/Header";
 import LetterList from "components/LetterList";
 import styled from "styled-components";
+import { Navigate } from "react-router-dom";
 
 export default function Home() {
   return (
     <Container>
+      <StDiv>
+        <StBtn onClick={() => {
+          <Navigate to = '/login'/>
+        }}>로그인</StBtn>
+        <StBtn onClick={() => {
+          <Navigate to = '/signup'/>
+        }}>회원가입</StBtn>
+      </StDiv>
       <Header />
       <AddForm />
       <LetterList />
@@ -18,3 +27,21 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+
+const StDiv = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`
+
+const StBtn = styled.button`
+  border-style: none;
+  background-color: #525252;
+  color: white;
+  font-weight: 500;
+  border-radius: 10px;
+  margin: 10px 10px 0px 5px;
+  width: 70px;
+  height: 30px;
+`
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,9 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Signup from "pages/Signup";
+import Profile from "pages/Profile";
+import PrivateRoute from "shared/PrivateRoute";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
@@ -8,6 +12,11 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+
+        {/* 위에 지정한 경로 외 경로로 접근하는 경우, Home 화면으로 이동 */}
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
